db480760487c4b8773c7b16940ea58c33e2e6bce
ramossinflores <mlaura.ragra@gmail.com>
2023-08-14 22:19:44 +0200

    Se agregó la lista del carrito de compras y se resolvieron posibles conflictos de sobreposición entre los menús en la versión desktop y mobile

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 const menuEmail =document.querySelector('.navbar-email');
-const destopMenu=document.querySelector('.desktop-menu');
+const desktopMenu=document.querySelector('.desktop-menu');
+const menuHamburgerIcon=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+const menuCartIcon=document.querySelector('.navbar-shopping-cart');
+const aside=document.querySelector('.product-detail');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgerIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click',toggleCartList);
 
 function toggleDesktopMenu (){
-    destopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+function toggleCartList(){
+    const isMobileMenuClosed=mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed=desktopMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+            mobileMenu.classList.add('inactive');}
+            aside.classList.toggle('inactive');    
+            
+    if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+        aside.classList.toggle('inactive');
+
+    }
 }
